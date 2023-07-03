696161da45c0b23a54f45bcb7b0836d165cb4440
arc1997 <askadeshchhajed@gmail.com>
2023-07-03 22:12:54 +0530

    Post Request In golang

--- 21webrequestsverb/main.go
+++ 21webrequestsverb/main.go
@@ -9,13 +9,48 @@ import (
 
 func main() {
 	fmt.Println("Welcome to web requests verb")
-	GetRequest()
+	//GetRequest()
+	PostRequest()
 }
 
 func GetRequest() {
 	const myurl = "http://localhost:8000/get"
 
 	response, error := http.Get(myurl)
+
+	if error != nil {
+		panic(error)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println(response.StatusCode)
+	var responseString strings.Builder
+	databyte, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	bytecount, _ := responseString.Write(databyte)
+	fmt.Println(bytecount)
+	fmt.Println(responseString.String())
+
+}
+
+func PostRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	// dummy json load
+
+	requestBody := strings.NewReader(`
+	{
+		"coursename" : "golang",
+		"paymentid" : "jhkd897",
+		"price" : 500
+	}
+	`)
+
+	response, error := http.Post(myurl, "application/json", requestBody)
 
 	if error != nil {
 		panic(error)
