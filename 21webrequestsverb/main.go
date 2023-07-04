0c04d2758834f8c441cd8c969e1cbe3037b93903
arc1997 <askadeshchhajed@gmail.com>
2023-07-04 19:02:18 +0530

    Form Data in golang

--- 21webrequestsverb/main.go
+++ 21webrequestsverb/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to web requests verb")
 	//GetRequest()
-	PostRequest()
+	//PostRequest()
+	FormDataRequest()
 }
 
 func GetRequest() {
@@ -51,6 +53,36 @@ func PostRequest() {
 	`)
 
 	response, error := http.Post(myurl, "application/json", requestBody)
+
+	if error != nil {
+		panic(error)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println(response.StatusCode)
+	var responseString strings.Builder
+	databyte, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	bytecount, _ := responseString.Write(databyte)
+	fmt.Println(bytecount)
+	fmt.Println(responseString.String())
+
+}
+
+func FormDataRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//dummy form data
+
+	formdata := url.Values{}
+	formdata.Add("course", "golang")
+	formdata.Add("price", "256")
+
+	response, error := http.PostForm(myurl, formdata)
 
 	if error != nil {
 		panic(error)
